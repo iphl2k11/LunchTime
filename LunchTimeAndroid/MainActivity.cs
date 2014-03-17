a5a7d3531fcdc6e6278b139a8a350e1ff6b5ff4f
Filip Franczyk <filip.franczyk@gmail.com>
2014-03-17 19:35:25 +0100

    android app added to project

---
+++ LunchTimeAndroid/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace LunchTimeAndroid
+{
+	[Activity (Label = "LunchTimeAndroid", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		int count = 1;
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			Button createEventButton = FindViewById<Button> (Resource.Id.createEvent);
+			Button notifyButton = FindViewById<Button> (Resource.Id.notify);
+			Button exitButton = FindViewById<Button> (Resource.Id.exit);
+
+			createEventButton.Click += delegate {
+				notifyButton.Text = string.Format ("Notify ({0})", count++);
+		
+				exitButton.Click += delegate{
+					SetResult(0);
+					Finish();
+				}
+
+			};
+		}
+	}
+}
+
+
